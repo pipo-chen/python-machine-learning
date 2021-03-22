54aa85ad2ba83e7b9bd31123d1502ada2856df05
mk2019 <unicorn.mark2019@gmail.com>
2021-03-22 22:56:13 +0800

    finish: 05 - 数据归一化

--- phase/playML/metrics.py
+++ phase/playML/metrics.py
@@ -3,5 +3,5 @@ import numpy as np
 def accuracy_socre(y_true, y_predict):
 	assert y_true.shape[0] == y_predict.shape[0],\
 	"the size of must y_true == y_predict"
-	return sum (y_true == y_predict) / len(y_true)
+	return sum(y_true == y_predict) / len(y_true)
 	
---
+++ phase/playML/preprocess.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class StandardScaler:
+	def __init__(self):
+		self.mean_ = None
+		self.scale_ = None
+
+
+	def fit(self, X):
+		#目前只处理 2 维
+		assert X.ndim == 2,'the dim of X must == 2'
+		self.mean_ = np.array([np.mean(X[:,i]) for i in range(X.shape[1])])
+		self.scale_ = np.array([np.std(X[:,i]) for i in range(X.shape[1])])
+		
+		return self
+
+	def transform(self, X):
+		assert X.ndim == 2,'the dim of X must == 2'
+		assert self.mean_ is not None and self.scale_ is not None,\
+		"must fit"
+		assert X.shape[1] == len(self.mean_),\
+		"the feature number of X must == mean_ and std_"
+
+		resX = np.empty(shape = X.shape, dtype = float)
+
+		#归一化每一列
+		for col in range(X.shape[1]):
+			resX[:,col] = (X[:,col] - self.mean_[col] / self.scale_[col])
+
+		return resX
+
+
+
+
+
